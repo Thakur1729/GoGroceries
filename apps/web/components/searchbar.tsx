e4bdda5151d775f11e17e93481e11f52ad2e8e69
Umesh Thakur <umeshthakur1729@gmail.com>
2025-03-05 13:24:16 +0530

    search bar modified

--- apps/web/components/searchbar.tsx
+++ apps/web/components/searchbar.tsx
@@ -18,7 +18,7 @@ function SearchBar() {
 
 				<input
 					className='w-full bg-transparent placeholder:text-slate-400 text-slate-800 text-sm border border-slate-200 rounded-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow'
-					placeholder='Milk...'
+					placeholder='Doodh...'
 				/>
 
 				<button
